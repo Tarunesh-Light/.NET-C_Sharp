04e0b2d0e0b76725def53fcab8fe87bd91f9f772
Tarunesh-Light <workassignments796796@gmail.com>
2023-10-06 23:12:11 +0530

    C Sharp

---
+++ Data Types/Data Types/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            string name = "Tarun";
+            char name1 = 'T';
+            double b = 2.089;
+            bool l = true;
+            Console.WriteLine("Hi your number is: " + num);
+            Console.WriteLine("Name is: " + name);
+            Console.WriteLine("Char is: " + name1);
+            Console.WriteLine("Float is: " + b);
+            Console.WriteLine("Bool is: " + l);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            string name = "Tarun";
+            Console.WriteLine("Hi your number is: " + num);
+            Console.WriteLine("Name is: " + name);
+            Console.ReadLine();
+        }
+    }
+}
