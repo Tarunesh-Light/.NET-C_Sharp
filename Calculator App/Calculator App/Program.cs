29a519f96d3fcb68c13f051d51c1c0b84946b80a
Tarunesh-Light <workassignments796796@gmail.com>
2023-10-07 19:19:33 +0530

    C Sharp

---
+++ Calculator App/Calculator App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number 1: ");
+            double num1=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
---
+++ Mad Lib/Mad Lib/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mad_Lib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, obj;
+            Console.Write("Enter colour: ");
+            color= Console.ReadLine();
+            Console.Write("Enter obj: ");
+            obj = Console.ReadLine();
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(obj + " are Blue");
+        }
+    }
+}
