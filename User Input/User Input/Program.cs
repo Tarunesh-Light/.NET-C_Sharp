35c14195579bb50962ff01b3e0e16600b271d992
Tarunesh-Light <workassignments796796@gmail.com>
2023-10-07 18:53:07 +0530

    C Sharp

---
+++ User Input/User Input/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Name is " + name);
+        }
+    }
+}
